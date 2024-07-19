f0f66b72503bddb0b8b3f392bbd773ad92348ce8
Marcelo Noguez <marcelonnoguez@gmail.com>
2024-07-19 19:23:44 -0300

    Add configuration files

---
+++ src/main/java/com/testecaju/TestecajuApplication.java
@@ -0,0 +1,13 @@
+package com.testecaju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestecajuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestecajuApplication.class, args);
+	}
+
+}
